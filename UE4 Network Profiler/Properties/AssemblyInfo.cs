f3d6505a8088ee0551fbd2e7cbc970addf5e32f4
ryanjon2040 <mindfreak2040@gmail.com>
2021-03-11 12:09:17 +0530

    Update to 1.1

--- UE4 Network Profiler/Properties/AssemblyInfo.cs
+++ UE4 Network Profiler/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UE4 Network Profiler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Unreal Engine Network Profiler")]
+[assembly: AssemblyDescription("WPF rewrite of WinForm based Network Profiler by Epic Games.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Satheesh (ryanjon2040)")]
 [assembly: AssemblyProduct("UE4 Network Profiler")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
